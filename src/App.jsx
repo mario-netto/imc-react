8d3eed59ba4b1920e37d9bcb673a04b004be9691
Mario Netto <marionettojr@gmail.com>
2024-11-23 03:10:56 -0300

    target input - ok / calc img - ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ function App() {
     const [altura, setAltura] = useState(0)
     const [peso, setPeso] = useState(0)
 
+    const calcImc = (peso /  ((altura /100 ) ** 2)).toFixed(1)
+
     return (
         <>
             <h1>Cacule seu IMC</h1>
             <form>
-                <input type="number" placeholder="Insira sua altura em cm"/>
-                <input type="number" placeholder="Insira seu peso em kl"/>
+                <input type="number" placeholder="Insira sua altura em cm" onKeyUp={e => setAltura(e.target.value)}/>
+                <input type="number" placeholder="Insira seu peso em kl" onKeyUp={e => setPeso(e.target.value)}/>
+                {calcImc}
             </form>
         </>
     )
